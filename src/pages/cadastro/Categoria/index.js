4b437981082e19e66eb6fe240f22dcb8697b752a
Polyana <polyanacsousa@gmail.com>
2020-07-30 22:11:40 -0300

    Correção do carousel

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,18 @@ function CadastroCategoria() {
     return (
       <PageDefault>
         <h1>Cadastre de Categoria</h1>
+        <form>
+          <label>
+            Nome da categoria:
+            <input 
+            type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
         <Link to="/">
           Ir para a home
